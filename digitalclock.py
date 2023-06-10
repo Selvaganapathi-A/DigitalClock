08597f005496d890e03d2b451013c5ed39a4ee5a
Selvaganapathi-A <selvaganapathi066@gmail.com>
2023-06-10 16:39:49 +0530

    add new functionality

--- digitalclock.py
+++ digitalclock.py
@@ -1,10 +1,25 @@
 from tkinter import Label, Tk
 
+import datetime
+import threading
+
 window: Tk = Tk()
 window.title("Digital Clock")
 window.geometry("640x320")
 window.configure(bg="orangered")
 
-label: Label = Label(window, font=("Poppins", 28, "bold"), bg="orangered", fg="white")
+label: Label = Label(window, font=("Poppins", 50, "bold"), bg="orangered", fg="white")
 label.pack(padx=10, pady=10)
+
+
+def clock():
+    time = datetime.datetime.now().strftime("%I:%M:%S %p")
+    label.configure(text=time)
+    label.after(500, clock)
+
+
+t1 = threading.Thread(target=clock)
+t1.setDaemon(True)
+t1.start()
+# clock()
 window.mainloop()
