b85b0a18ae1fdfd4654863f7de4d6c32648ef26d
Selvaganapathi-A <selvaganapathi066@gmail.com>
2023-06-10 16:31:29 +0530

    removed welcome Label

--- digitalclock.py
+++ digitalclock.py
@@ -5,8 +5,6 @@ window.title("Digital Clock")
 window.geometry("640x320")
 window.configure(bg="orangered")
 
-label: Label = Label(
-    window, text="Welcome", font=("Poppins", 28, "bold"), bg="orangered", fg="white"
-)
+label: Label = Label(window, font=("Poppins", 28, "bold"), bg="orangered", fg="white")
 label.pack(padx=10, pady=10)
 window.mainloop()
