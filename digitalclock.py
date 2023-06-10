4c0b54a160e99b0f5ecb938785c26ba87f51c509
Selvaganapathi-A <selvaganapathi066@gmail.com>
2023-06-10 16:30:53 +0530

    Add Label

--- digitalclock.py
+++ digitalclock.py
@@ -1,7 +1,12 @@
-from tkinter import Tk
+from tkinter import Label, Tk
 
 window: Tk = Tk()
 window.title("Digital Clock")
 window.geometry("640x320")
+window.configure(bg="orangered")
 
+label: Label = Label(
+    window, text="Welcome", font=("Poppins", 28, "bold"), bg="orangered", fg="white"
+)
+label.pack(padx=10, pady=10)
 window.mainloop()
